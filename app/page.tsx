1bfc41dee3a23833f2ad7580bc3306d6d1011f6a
chunwen <hsbsbbdn@gmail.com>
2024-05-18 01:17:26 +0800

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from './ui/home.module.css';
+import styles2 from './ui/home2.module.css';
+
+import Image from 'next/image';
+import { lusitana } from './ui/font';
 
 export default function Page() {
   return (
@@ -8,9 +12,13 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
+      <div className={styles.shape} />
+      <div className={styles2.shape} />
+
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+        <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/>
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.style}`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -26,7 +34,14 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            alt="Acme Hero Image"
+            width={500}
+            height={500}  
+          />
         </div>
+        
       </div>
     </main>
   );
