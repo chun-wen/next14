f3e83263ac3ecfadae622aa9dcef9e39e3ea4f6b
chunwen <hsbsbbdn@gmail.com>
2024-05-20 00:34:14 +0800

    feat: add searching page

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,24 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  
+  const handleSearch = (event: string) => {
+    const params = new URLSearchParams(searchParams);
+    if(event){
+      params.set('query', event);
+    }
+    else{
+      params.delete('query')
+    }
+    replace(`${pathname}?${params.toString()}`);
+    console.log("🚀 ~ handleSearch ~ params:", pathname)
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +27,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(event) => handleSearch(event.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
