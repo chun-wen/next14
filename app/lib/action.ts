f2530e79b7fde99df1f607eb6a2cae57eec4aec1
chunwen <hsbsbbdn@gmail.com>
2024-05-26 12:27:32 +0800

    feat: add login and logout ui and flow

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,5 +1,7 @@
 'use server';
+import { signIn } from '@/auth';
 import { sql } from '@vercel/postgres';
+import { AuthError } from 'next-auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
@@ -106,3 +108,22 @@ export async function deleteInvoice(id: string) {
     console.error('Error deleting invoice:', error);
   }
 }
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+} 
