c0a0a25decca00bf189843d234987af4110153dc
chunwen <hsbsbbdn@gmail.com>
2024-05-28 00:25:43 +0800

    feat: add metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,12 +1,17 @@
+import { fetchInvoicesPages } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import Pagination from '@/app/ui/invoices/pagination';
 import Table from '@/app/ui/invoices/table';
 import Search from '@/app/ui/search';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
-import Pagination from '@/app/ui/invoices/pagination';
-
+ 
+export const metadata: Metadata = {
+  title: 'Invoices | Acme Dashboard',
+  description: 'View and manage your invoices.'
+};
  
 export default async function Page({
   searchParams,
